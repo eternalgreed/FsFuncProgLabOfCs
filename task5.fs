de17e917b4e657230db5ddf167eb562831114e15
Ramazan Abdullaev <eternalgreed99@gmail.com>
2026-09-26 15:43:49 +0300

    Task 5 added

--- task5.fs
+++ task5.fs
@@ -1,10 +1,10 @@
 // 16.1
-let notDivisible (n, m) = m % n <> 0
+let notDivisible (n, m) = m % n = 0
 
 // 16.2
 let prime n =
     let rec test divisor =
         divisor >= n
-        || (notDivisible (divisor, n) && test (divisor + 1))
+        || (not (notDivisible (divisor, n)) && test (divisor + 1))
 
     n >= 2 && test 2
